3a732b01a465e9970217f1c20a7957d9121c84be
chenyanqiu <chenyanqiu@chinacloud.com.cn>
2017-11-29 15:21:51 +0800

    springboot集成neo4j的demo

---
+++ src/main/java/com/chinacloud/DataBloodApplication.java
@@ -0,0 +1,12 @@
+package com.chinacloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataBloodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataBloodApplication.class, args);
+	}
+}
---
+++ src/main/java/com/chinacloud/dao/Neo4jDao.java
@@ -0,0 +1,14 @@
+package com.chinacloud.dao;/**
+ * Created by Administrator on 2017/11/29.
+ */
+
+import org.springframework.stereotype.Repository;
+
+/**
+ * @author Administrator
+ * @date 2017/11/29
+ **/
+
+@Repository
+public interface Neo4jDao {
+}
